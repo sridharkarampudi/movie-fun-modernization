edfcecae6cabcee92448d79f6b9ca6ac653aad7d
Common Training user <CT0t1868@TechMahindra.com>
2019-06-05 19:08:15 +0530

    multi jar - modernysation

---
+++ components/albums/src/test/java/org/superbiz/moviefun/albums/AlbumTest.java
@@ -0,0 +1,20 @@
+package org.superbiz.moviefun.albums;
+
+import org.hamcrest.core.Is;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class AlbumTest {
+
+    @Test
+    public void testIsEquivalent() {
+        Album persisted = new Album("Radiohead", "OK Computer", 1997, 8);
+        persisted.setId(10);
+
+        Album sameFromCsv = new Album("Radiohead", "OK Computer", 1997, 9);
+        Assert.assertThat(persisted.isEquivalent(sameFromCsv), Is.is(true));
+
+        Album otherFromCsv = new Album("Radiohead", "Kid A", 2000, 9);
+        Assert.assertThat(persisted.isEquivalent(otherFromCsv), Is.is(false));
+    }
+}
